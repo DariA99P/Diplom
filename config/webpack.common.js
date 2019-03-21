b297edf6ad4fe48d74ec22cbff11afe0513308ec
Fernando Silva <Fernando.Silva@vistajet.com>
2019-03-21 19:13:55 -0300

    [UIBOILER-47] - Add new boilerplate index.html and config

---
+++ config/webpack.common.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const GenerateJsonPlugin = require('generate-json-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const keycloak = require('../keycloak.json');
+
+module.exports = {
+    entry: {
+        main: ["babel-polyfill", path.resolve(__dirname, "../src", "index.js")],
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, '../build'),
+        publicPath: "/"
+    },
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        overlay: true,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/],
+                use: [{ loader: "babel-loader" }]
+            },
+            {
+                test: /.*\.(gif|png|jp(e*)g|svg)$/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 21000,
+                            name: "images/[name]_[hash:7].[ext]"
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, '../public', 'index.html'),
+        }),
+        new GenerateJsonPlugin('keycloak.json', keycloak),
+        // Copy environment configuration file to the production bundle
+        new CopyWebpackPlugin([{
+          from: path.resolve(__dirname, "../", "env.js"),
+          to: 'env.js'
+        }]),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+}
---
+++ config/webpack.dev.js
@@ -0,0 +1,54 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mapStyle = process.env.MAP_STYLE === 'true';
+
+module.exports = merge (common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        overlay: true,
+        open: true,
+        stats: 'errors-only'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    {
+                        loader: mapStyle ? "css-loader?sourceMap" : "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                            camelCase: true,
+                        },
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            ident: 'postcss',
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+    ],
+});
