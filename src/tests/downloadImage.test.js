b703534fff8b5b34a13021d6670b18c1cf2ccfea
Poprotskaya <daria.poprotskaya@vistajet.com>
2020-05-06 20:02:28 +0300

    comment tests

--- src/tests/downloadImage.test.js
+++ src/tests/downloadImage.test.js
@@ -1,32 +1,32 @@
-describe('Client tests', () => {
-  it('Download image - Should return object file', () => {
-    expect(true).toEqual(true);
-  });
-  it('Remove image from my drawings - Should return new object', () => {
-    expect(true).toEqual(true);
-  });
-  it('Save image to My drawings - Should return new object', () => {
-    expect(true).toEqual(true);
-  });
-  it('User subscription function - Should return new subscriptions list', () => {
-    expect(true).toEqual(true);
-  });
-  it('User unsubscription function - Should return new subscribers list', () => {
-    expect(true).toEqual(true);
-  });
-  it('Save to device function - Should return answer that the image is saved', () => {
-    expect(true).toEqual(true);
-  });
-  it('Mutual Subscription Function - Should return valid list', () => {
-    expect(true).toEqual(true);
-  });
-  it('Shape recognition function - depending on the server’s response, return the necessary shape type for rendering', () => {
-    expect(true).toEqual(true);
-  });
-  it('Function for saving user information - to form data as the backs expects them', () => {
-    expect(true).toEqual(true);
-  });
-  it('Photo change function - after successful development of the function should return the true', () => {
-    expect(true).toEqual(true);
-  });
-});
+// describe('Client tests', () => {
+//   it('Download image - Should return object file', () => {
+//     expect(true).toEqual(true);
+//   });
+//   it('Remove image from my drawings - Should return new object', () => {
+//     expect(true).toEqual(true);
+//   });
+//   it('Save image to My drawings - Should return new object', () => {
+//     expect(true).toEqual(true);
+//   });
+//   it('User subscription function - Should return new subscriptions list', () => {
+//     expect(true).toEqual(true);
+//   });
+//   it('User unsubscription function - Should return new subscribers list', () => {
+//     expect(true).toEqual(true);
+//   });
+//   it('Save to device function - Should return answer that the image is saved', () => {
+//     expect(true).toEqual(true);
+//   });
+//   it('Mutual Subscription Function - Should return valid list', () => {
+//     expect(true).toEqual(true);
+//   });
+//   it('Shape recognition function - depending on the server’s response, return the necessary shape type for rendering', () => {
+//     expect(true).toEqual(true);
+//   });
+//   it('Function for saving user information - to form data as the backs expects them', () => {
+//     expect(true).toEqual(true);
+//   });
+//   it('Photo change function - after successful development of the function should return the true', () => {
+//     expect(true).toEqual(true);
+//   });
+// });
